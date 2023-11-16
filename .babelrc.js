4abb40c5dc021c17ca9f5f13a34f8ab1572a5134
Ryan McCue <me@ryanmccue.info>
2023-11-16 16:09:01 +0000

    Add support for TypeScript

--- .babelrc.js
+++ .babelrc.js
@@ -1,1 +1,6 @@
-module.exports = require( '@humanmade/webpack-helpers/babel-preset' );
+module.exports = {
+    presets: [
+        '@babel/preset-typescript',
+        '@humanmade/webpack-helpers/babel-preset'
+    ],
+};
--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -1,5 +1,10 @@
-const { helpers, externals, presets } = require( '@humanmade/webpack-helpers' );
+const { helpers, externals, loaders, presets } = require( '@humanmade/webpack-helpers' );
+const postcssNesting = require( 'postcss-nesting' );
 const { choosePort, filePath } = helpers;
+
+// Mutate the loader defaults.
+loaders.ts.defaults.loader = 'babel-loader';
+loaders.postcss.defaults.options.postcssOptions.plugins.push( postcssNesting() );
 
 module.exports = choosePort( 3000 ).then( port => (
 	presets.development( {
@@ -14,6 +19,16 @@ module.exports = choosePort( 3000 ).then( port => (
 		output: {
 			path: filePath( 'build' ),
 			publicPath: `https://localhost:${ port }/`,
+		},
+		resolve: {
+			extensions: [
+				'.ts',
+				'.tsx',
+				'.wasm',
+				'.mjs',
+				'.js',
+				'.json',
+			],
 		},
 	} )
 ) );
